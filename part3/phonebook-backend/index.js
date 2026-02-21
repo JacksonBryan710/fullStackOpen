36a64638673f31c5acbcf21d565cb7a5d09c2df2
jacksonbryan710 <jacksonbryan710@gmail.com>
2026-02-20 19:12:54 -0500

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json());
 
 morgan.token('body', function (req, res) {
-    return JSON.stringify(req.body)
-})
+    return JSON.stringify(req.body);
+});
 
 app.use(
     morgan(function (tokens, req, res) {
@@ -103,7 +103,7 @@ app.get('/info', (request, response) => {
         `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
